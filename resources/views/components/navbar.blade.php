53aa7c6a7873bf58c7bbf4031679e5e58eb88e29
mkdgarp <natdanaichorchamp1@hotmail.com>
2023-02-20 21:19:33 +0700

    asdasd

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,11 +3,11 @@
 
         <a class="navbar-brand" href="/home" tabindex="-1">
             {{-- <img src="../../img/logo.jpg" width="40px" class="d-inline-block align-text-top" /> --}}
-            {{-- <span class="namesv">Eden Craft<br><small>my shop</small></span> --}}
+            {{-- <span class="namesv"></small></span> --}}
             myshop
         </a>
         <?php
-        $data_tooltip = '<b>EdenCraft</b><br>เซิฟเวอร์มายคราฟ ที่เน้น สังคม คอมมูนิตี้สำหรับทุกเพศทุกวัย!<br> ';
+        $data_tooltip = '';
         ?>
         <div class="float-end" id="question-svg" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-html="true"
             title="<?php echo $data_tooltip; ?>">
